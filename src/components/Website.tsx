65ad67d5b6218763c9cdb06017d5fc257759b162
Ahamed Safnas <129050137+safnasNED25@users.noreply.github.com>
2025-09-05 22:06:51 +0530

    Updated Website.tsx

--- src/components/Website.tsx
+++ src/components/Website.tsx
@@ -38,6 +38,7 @@ import { useState } from 'react';
 import { Service, ContactInfo } from '../types';
 import TestimonialSection from './TestimonialSection';
 import { useVisitorTracking } from '../hooks/useVisitorTracking';
+import TikTokIcon from './TikTokIcon';
 
 interface WebsiteProps {
   services: Service[];
@@ -552,7 +553,7 @@ export default function Website({ services, contactInfo, testimonials, onVisit }
                   onClick={() => handleSocialClick(contactInfo.socialMedia.tiktok)}
                   className="text-gray-400 hover:text-white transition-colors flex items-center space-x-1"
                 >
-                  <Music className="w-5 h-5" />
+                  <TikTokIcon className="w-5 h-5" />
                 </button>
                 <button 
                   onClick={() => handleSocialClick(contactInfo.socialMedia.email)}
